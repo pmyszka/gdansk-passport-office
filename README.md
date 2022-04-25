e9e2baf0a8375439a3bee42e076bac632075146b
Piotr Myszka <pmyszka@users.noreply.github.com>
2022-04-25 11:39:44 +0200

    Updated README and used google code formatter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # gdansk-passport-office
+
+1. Create an `.env` file containing Pushover credentials
+2. Replace `branch_id` and `service_id` values in `appointment_scheduler.py` according to your desired office branch
+3. Install dependencies
+```
+$ pip install -r requirements.txt
+```
+4. Run the script
+```
+$ python appointment_scheduler.py
+```
--- notifiers.py
+++ notifiers.py
@@ -3,13 +3,13 @@ from dotenv import dotenv_values
 
 
 class PushoverNotifier:
-  url = 'https://api.pushover.net/1/messages.json'
+    url = 'https://api.pushover.net/1/messages.json'
 
-  def __init__(self):
-    config = dotenv_values()
-    self.token = config['PUSHOVER_TOKEN']
-    self.user = config['PUSHOVER_USER']
+    def __init__(self):
+        config = dotenv_values()
+        self.token = config['PUSHOVER_TOKEN']
+        self.user = config['PUSHOVER_USER']
 
-  def notify(self, payload):
-    requests.post(self.url, data={'token': self.token, 'user': self.user,
-                                  'message': payload})
+    def notify(self, payload):
+        requests.post(self.url, data={'token': self.token, 'user': self.user,
+                                      'message': payload})
